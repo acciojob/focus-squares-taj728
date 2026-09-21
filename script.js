12849979f91a966eedce9aa142e3e5fd3d6a7222
taj728 <tajaleemsait20@gmail.com>
2026-09-21 20:00:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-//your JS code here. If required.
+const boxes=document.querySelectorAll(".square");
+
+boxes.forEach((x)=>{
+    x.addEventListener("mouseenter",()=>{
+        boxes.forEach((y)=>{
+            if(x===y){
+                 
+            }
+            else{
+                y.style.backgroundColor="#6F4E37"
+            }
+        })
+    })
+    x.addEventListener("mouseleave",()=>{
+        boxes.forEach((y)=>{
+            y.style.backgroundColor="#E6E6FA"
+        })
+    })
+})
+
